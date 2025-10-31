56a1ad3efc6bf0587149a897a445b2ec9bf55501
OrangeQu <2944949479@qq.com>
2025-10-31 15:14:58 +0800

    Merge remote changes

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <h1 id="title">{{ message }}</h1> -->
+    <!-- <test-button></test-button> -->
+    <!-- <user-view></user-view> -->
+    <!-- <question-view></question-view> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import QuestionView from './views/element/QuestionView.vue';
+// import TestButton from './views/element/TestButton.vue';
+// import UserView from './views/element/UserView.vue';
+
+export default {
+  // components:{ QuestionView},
+  data() {
+    return {
+      message: "hello vue",
+    };
+  },
+};
+</script>
+
+<style>
+#title {
+  color: red;
+}
+</style>
